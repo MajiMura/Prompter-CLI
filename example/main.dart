7139805d0593e1ead3247c57f7bf158e3cd9c6a0
Joshua Green <joshuagreen@Joshuas-MacBook-Pro.local>
2021-11-29 01:15:31 +0000

    feat: Initial commit

---
+++ example/main.dart
@@ -0,0 +1,17 @@
+import 'package:prompter_cli/prompter_cli.dart';
+
+void main() {
+  final options = [
+    Option('I want red', '#f00'),
+    Option('I want blue', '#00f'),
+  ];
+
+  final prompter = Prompter();
+
+  var colorCode = prompter.askMultiple('Select a color', options);
+
+  bool answer = prompter.askBinary('Do you like this lib?');
+
+  print(colorCode);
+  print(answer);
+}
